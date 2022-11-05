42c65c7bf7bd1148749eb93c4d554bf00660badc
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-05 14:56:32 +0530

    Create Rotate90_ACW.cpp

---
+++ PS_Programs/Rotate90_ACW.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate90cw(int nums[][100], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            swap(nums[i][j], nums[j][i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums, nums + n);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int nums[100][100];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> nums[i][j];
+        }
+    }
+
+    rotate90cw(nums, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << nums[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
