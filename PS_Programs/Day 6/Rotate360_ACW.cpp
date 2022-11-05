4946b9b18f3b0b66c0ba961562bc02686037371e
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-05 15:12:08 +0530

    Create Rotate360_ACW.cpp

---
+++ PS_Programs/Day 6/Rotate360_ACW.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool rotate360acw(int nums[][100], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums[i], nums[i] + n);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums, nums + n);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums[i], nums[i] + n);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums, nums + n);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int nums[100][100];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> nums[i][j];
+        }
+    }
+
+    rotate360acw(nums, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << nums[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
