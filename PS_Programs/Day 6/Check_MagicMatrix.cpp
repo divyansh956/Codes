90e4f1adca8a60543d1fa0d5c1ec246cec23eeab
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-05 15:15:49 +0530

    Update Check_MagicMatrix.cpp

--- PS_Programs/Day 6/Check_MagicMatrix.cpp
+++ PS_Programs/Day 6/Check_MagicMatrix.cpp
@@ -3,66 +3,66 @@ using namespace std;
 
 int main()
 {
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  int nums[n][n];
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
+    int nums[n][n];
+    for (int i = 0; i < n; i++)
     {
-      cin >> nums[i][j];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> nums[i][j];
+        }
     }
-  }
 
-  unordered_map<int, int> mp;
+    unordered_map<int, int> mp;
 
-  int sum = 0;
+    int sum = 0;
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++)
     {
-      sum += nums[i][j];
+        for (int j = 0; j < n; j++)
+        {
+            sum += nums[i][j];
+        }
+        mp[sum]++;
+        sum = 0;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sum += nums[j][i];
+        }
+        mp[sum]++;
+        sum = 0;
+    }
+
+    int sum1 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                sum += nums[i][j];
+            }
+            if (i + j == n - 1)
+            {
+                sum1 += nums[i][j];
+            }
+        }
     }
     mp[sum]++;
-    sum = 0;
-  }
+    mp[sum1]++;
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
+    if (mp.size() == 1)
     {
-      sum += nums[j][i];
+        cout << "Magic Matrix" << endl;
     }
-    mp[sum]++;
-    sum = 0;
-  }
-
-  int sum1 = 0;
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
+    else
     {
-      if (i == j)
-      {
-        sum += nums[i][j];
-      }
-      if (i + j == n - 1)
-      {
-        sum1 += nums[i][j];
-      }
+        cout << "Not Magic Matrix" << endl;
     }
-  }
-  mp[sum]++;
-  mp[sum1]++;
-
-  if (mp.size() == 1)
-  {
-    cout << "Magic Matrix" << endl;
-  }
-  else
-  {
-    cout << "Not Magic Matrix" << endl;
-  }
 }
