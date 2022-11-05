2a036ed799ffa027ae9fb3ded9a9df26f3ac6f27
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-05 15:02:42 +0530

    Create Rotate180.cpp

---
+++ PS_Programs/Day 6/Rotate180.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate90cw(int nums[][100], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums, nums + n);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        reverse(nums[i], nums[i] + n);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int nums[100][100];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> nums[i][j];
+        }
+    }
+
+    rotate90cw(nums, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << nums[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
