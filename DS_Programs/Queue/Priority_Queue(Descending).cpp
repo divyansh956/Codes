df3a52922aca0dda7218e0474544a959a90e5176
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-06 16:40:20 +0530

    Create Priority_Queue(Descending).cpp

---
+++ DS_Programs/Queue/Priority_Queue(Descending).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin >> n;
+
+   int nums[n];
+   priority_queue<int, vector<int>, greater<int>> pq;
+
+   for (int i = 0; i < n; i++)
+   {
+      cin >> nums[i];
+      pq.push(nums[i]);
+   }
+
+   while (!pq.empty())
+   {
+      cout << pq.top() << " ";
+      pq.pop();
+   }
+   cout << endl;
+}
