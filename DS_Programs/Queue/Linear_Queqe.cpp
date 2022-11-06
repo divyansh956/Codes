9147b91401c243ee4e3747aeb7280d401210085b
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-06 14:52:29 +0530

    Create Linear_Queqe.cpp

---
+++ DS_Programs/Queue/Linear_Queqe.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#define QUEUQSIZE 100
+#define true 1
+#define false 0
+
+struct Queue
+{
+   int item[QUEUQSIZE];
+   int rear;
+   int front;
+};
+struct Queue q;
+
+void Initialize()
+{
+   q.rear = -1;
+}
+
+int isEmpty()
+{
+   if (q.rear == -1)
+   {
+      return 1;
+   }
+   return 0;
+}
+
+void push(int x)
+{
+   if (q.rear == QUEUQSIZE - 1)
+   {
+      printf("Queue Overflow");
+      return;
+   }
+   q.rear++;
+   q.item[q.rear] = x;
+}
+
+int pop()
+{
+   int x;
+   if (isEmpty())
+   {
+      printf("Queue Underflows");
+      return -1;
+   }
+   x = q.item[q.front];
+   q.front++;
+   return x;
+}
+
+int front()
+{
+   if (isEmpty())
+   {
+      printf("Queue Is Empty");
+      return -1;
+   }
+   return q.item[q.front];
+}
+
+int main()
+{
+   int x;
+   Initialize();
+   push(200);
+   push(100);
+   push(300);
+   push(400);
+   pop();
+   x = front();
+   printf("%d", x);
+}
