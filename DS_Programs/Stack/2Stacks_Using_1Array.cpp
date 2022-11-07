ee4230c51e750d62ffc7fdc6b471692c25bfb962
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-07 09:54:25 +0530

    Update 2Stacks_Using_1Array.cpp

--- DS_Programs/Stack/2Stacks_Using_1Array.cpp
+++ DS_Programs/Stack/2Stacks_Using_1Array.cpp
@@ -1,94 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define n 100
 
 class twoStacks
 {
-    int *arr;
-    int size;
-    int top1, top2;
+   int *arr;
+   int size;
+   int top1, top2;
 
 public:
-    twoStacks(int n)
-    {
-        size = n;
-        arr = new int[n];
-        top1 = n / 2 + 1;
-        top2 = n / 2;
-    }
+   twoStacks()
+   {
+      size = n;
+      arr = new int[n];
+      top1 = -1;
+      top2 = n;
+   }
 
-    void push1(int x)
-    {
-        if (top1 > 0)
-        {
-            top1--;
-            arr[top1] = x;
-        }
-        else
-        {
-            cout << "Stack Overflow"
-                 << " By element : " << x << endl;
-            return;
-        }
-    }
+   void push1(int x)
+   {
+      if (top2 == top1)
+      {
+         cout << "Stack Overflow"
+              << " By element : " << x << endl;
+         return;
+      }
+      else
+      {
+         top1++;
+         arr[top1] = x;
+      }
+   }
 
-    void push2(int x)
-    {
-        if (top2 < size - 1)
-        {
-            top2++;
-            arr[top2] = x;
-        }
-        else
-        {
-            cout << "Stack Overflow"
-                 << " By element : " << x << endl;
-            return;
-        }
-    }
+   void push2(int x)
+   {
+      if (top2 == top1)
+      {
+         cout << "Stack Overflow"
+              << " By element : " << x << endl;
+         return;
+      }
+      else
+      {
+         top2--;
+         arr[top2] = x;
+      }
+   }
 
-    int pop1()
-    {
-        if (top1 <= size / 2)
-        {
-            int x = arr[top1];
-            top1++;
-            return x;
-        }
-        else
-        {
-            cout << "Stack UnderFlow";
-            exit(1);
-        }
-    }
+   int pop1()
+   {
+      if (top1 == -1)
+      {
+         cout << "Stack UnderFlow" << endl;
+         exit(1);
+      }
+      else
+      {
+         int x = arr[top1];
+         top1--;
+         return x;
+      }
+   }
 
-    int pop2()
-    {
-        if (top2 >= size / 2 + 1)
-        {
-            int x = arr[top2];
-            top2--;
-            return x;
-        }
-        else
-        {
-            cout << "Stack UnderFlow" << endl;
-            exit(1);
-        }
-    }
+   int pop2()
+   {
+      if (top2 == n)
+      {
+         cout << "Stack UnderFlow" << endl;
+         exit(1);
+      }
+      else
+      {
+         int x = arr[top2];
+         top2++;
+         return x;
+      }
+   }
 };
 
 int main()
 {
-    twoStacks ts(5);
-    ts.push1(5);
-    ts.push2(10);
-    ts.push2(15);
-    ts.push1(11);
-    ts.push2(7);
-    cout << "Popped element from stack1 is "
-         << ": " << ts.pop1() << endl;
-    ts.push2(40);
-    cout << "Popped element from stack2 is "
-         << ": " << ts.pop2() << endl;
-    return 0;
+   twoStacks ts;
+   ts.push1(5);
+   ts.push2(10);
+   ts.push2(15);
+   ts.push1(11);
+   ts.push2(7);
+   cout << "Popped element from stack1 is "
+        << ": " << ts.pop1() << endl;
+   ts.push2(40);
+   cout << "Popped element from stack2 is "
+        << ": " << ts.pop2() << endl;
+   ts.pop1();
+   ts.pop1();
+   return 0;
 }
