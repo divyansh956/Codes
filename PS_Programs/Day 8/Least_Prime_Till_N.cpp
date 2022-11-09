94b0bd00641978d5bd4909b8920d2009412f103b
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-09 15:10:16 +0530

    Create Least_Prime_Till_N.cpp

---
+++ PS_Programs/Day 8/Least_Prime_Till_N.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            cout << i << " ";
+        }
+
+        for (int j = 2; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                cout << j << " ";
+                break;
+            }
+        }
+    }
+}
