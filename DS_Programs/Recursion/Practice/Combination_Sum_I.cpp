5830195957aa9bd44047637322aabd9fd0634531
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-13 15:17:47 +0530

    Create Combination_Sum_I.cpp

---
+++ DS_Programs/Recursion/Practice/Combination_Sum_I.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds)
+    {
+        if (ind == arr.size())
+        {
+            if (target == 0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if (arr[ind] <= target)
+        {
+            ds.push_back(arr[ind]);
+            findCombination(ind, target - arr[ind], arr, ans, ds);
+            ds.pop_back();
+        }
+
+        findCombination(ind + 1, target, arr, ans, ds);
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(0, target, candidates, ans, ds);
+        return ans;
+    }
+};
+
+
+int main()
+{
+    Solution obj;
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    vector<vector<int>> ans = obj.combinationSum(v, k);
+
+    cout << "Combinations are: " << endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
