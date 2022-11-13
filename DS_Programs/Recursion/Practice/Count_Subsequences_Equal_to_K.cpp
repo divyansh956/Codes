e3c89fb643f19170ba8b56f8cab280c36921a4f7
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-13 12:09:58 +0530

    Create Count_Subsequences_Equal_to_K.cpp

---
+++ DS_Programs/Recursion/Practice/Count_Subsequences_Equal_to_K.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subsequence(int nums[], int i, int n, int sum, int k)
+{
+    if (i == n)
+    {
+        if (sum == k)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    ans.push_back(nums[i]);
+    sum += nums[i];
+
+    int l = subsequence(nums, i + 1, n, sum, k);
+
+    ans.pop_back();
+    sum -= nums[i];
+
+    int r = subsequence(nums, i + 1, n, sum, k);
+    return l + r;
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    vector<int> ans;
+
+    cout << subsequence(nums, 0, n, 0, k) << endl;
+}
