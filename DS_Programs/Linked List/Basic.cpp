4c339ddb7832cffcd27e1b9ce9ed1ebddee3561e
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-17 10:42:48 +0530

    Update Basic.cpp

--- DS_Programs/Linked List/Basic.cpp
+++ DS_Programs/Linked List/Basic.cpp
@@ -11,15 +11,26 @@ struct node *start;
 
 struct node *GetNode()
 {
+
      struct node *p;
      p = start;
      p = (struct node *)malloc(sizeof(struct node *));
      return p;
 }
 
-void InsEnd(int x)
+void InsAft(struct node **q, int x)
 {
-     struct node *q = start;
+     struct node *p, *r;
+     p = GetNode();
+     p->info = x;
+     r = (*q)->next;
+     p->next = r;
+     (*q)->next = p;
+}
+
+void InsEnd(struct node **start, int x)
+{
+     struct node *q = *start;
      while (q->next != NULL)
      {
           q = q->next;
@@ -30,36 +41,51 @@ void InsEnd(int x)
      q->next = p;
 }
 
-void InsBeg(int x)
+void InsBeg(struct node **start, int x)
 {
      struct node *p;
      p = GetNode();
      p->info = x;
-     p->next = start;
-     start = p;
+     p->next = *start;
+     *start = p;
 }
 
-void traverse()
+void traverse(struct node *start)
 {
      struct node *p;
      p = start;
      while (p != NULL)
      {
-          printf("%d->", p->info);
+          printf("%d ", p->info);
           p = p->next;
      }
 }
 
 int main()
 {
-     start = NULL;
-     InsBeg(100);
-     InsBeg(200);
-     InsBeg(300);
-     InsBeg(500);
-     InsBeg(100);
-     InsBeg(300);
-     InsEnd(1000);
-     InsEnd(600);
-     traverse();
+     struct node *start = NULL;
+     InsBeg(&start, 100);
+     InsBeg(&start, 23);
+     InsBeg(&start, 300);
+     InsBeg(&start, 500);
+     InsBeg(&start, 97);
+     InsBeg(&start, 300);
+     InsEnd(&start, 1000);
+
+     struct node *p = start;
+     printf("Enter Positon to be Inserted: ");
+
+     int idx;
+     scanf("%d", &idx);
+
+     for (int i = 1; i < idx; i++)
+     {
+          p = p->next;
+     }
+
+     InsEnd(&start, 39);
+     InsAft(&p, 40);
+
+     printf("Linked List: ");
+     traverse(start);
 }
