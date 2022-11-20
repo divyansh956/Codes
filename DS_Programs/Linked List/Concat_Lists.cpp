96793b51dd2f7778476367f9557dab6e047f0a10
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-11-20 07:32:05 +0530

    Create Concat_Lists.cpp

---
+++ DS_Programs/Linked List/Concat_Lists.cpp
@@ -0,0 +1,183 @@
+// Using 1 Based Indexing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    n->next = head;
+    head = n;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAfter(node *&head, int val, int idx)
+{
+    node *n = new node(val);
+
+    if (idx == 0)
+    {
+        insertAtHead(head, val);
+        return;
+    }
+
+    node *temp = head;
+
+    int i = 1;
+    for (i = 1; i < idx && temp->next != NULL; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (idx == i - 1)
+    {
+        insertAtTail(head, val);
+        return;
+    }
+
+    if (idx != i)
+    {
+        cout << "Index " << idx << " Out of Range : Linked List Size = " << i << endl;
+        return;
+    }
+
+    n->next = temp->next;
+    temp->next = n;
+}
+
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+
+    head = head->next;
+    delete todelete;
+}
+
+void deleteVal(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        cout << "Linked List Is Empty" << endl;
+        return;
+    }
+
+    if (head->data == val || head->next == NULL)
+    {
+        if (head->data != val)
+        {
+            cout << "Element: " << val << " Not Found" << endl;
+            return;
+        }
+
+        deleteAtHead(head);
+        return;
+    }
+
+    node *temp = head;
+
+    while (temp->next->data != val && temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    if (temp->next->next == NULL && temp->next->data != val)
+    {
+        cout << "Element: " << val << " Not Found" << endl;
+        return;
+    }
+
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
+void concate(node *&head3, node *head1, node *head2)
+{
+    node *temp1 = head1;
+    node *temp2 = head2;
+
+    while (temp1 != NULL)
+    {
+        insertAtTail(head3, temp1->data);
+        temp1 = temp1->next;
+    }
+
+    while (temp2 != NULL)
+    {
+        insertAtTail(head3, temp2->data);
+        temp2 = temp2->next;
+    }
+}
+
+void display(node *head)
+{
+    if (head == NULL)
+    {
+        cout << "Linked List Is Empty" << endl;
+        return;
+    }
+
+    cout << "Linked List: ";
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    node *head1 = NULL;
+    node *head2 = NULL;
+    node *head3 = NULL;
+
+    insertAtTail(head1, 1);
+    insertAtTail(head2, 2);
+    insertAtTail(head1, 3);
+    insertAtHead(head2, 4);
+    insertAtHead(head1, 5);
+    deleteVal(head1, 3);
+    insertAtHead(head1, 9);
+    insertAtTail(head1, 6);
+    insertAfter(head2, 10, 1);
+    insertAtTail(head1, 11);
+    display(head1);
+    display(head2);
+
+    concate(head3, head1, head2);
+    display(head3);
+}
