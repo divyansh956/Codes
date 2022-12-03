999c6f9a240dfe261f0dd67137d9274b50c0a30a
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-03 06:48:22 +0530

    Create Find_Kth_Node_From_Last.cpp

---
+++ DS_Programs/Linked List/Find_Kth_Node_From_Last.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    n->next = head;
+    head = n;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    if (head == NULL)
+    {
+        cout << "Linked List Is Empty" << endl;
+        return;
+    }
+
+    cout << "Linked List: ";
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+node *findkthNodefromLast(node *&head, int k)
+{
+    node *p = head;
+    node *q = head;
+
+    while (q != NULL && k--)
+    {
+        q = q->next;
+    }
+
+    if (q == NULL && k == 0)
+    {
+        return head;
+    }
+    else if (q == NULL && k > 0)
+    {
+        cout << "Element Does Not Exist" << endl;
+        return NULL;
+    }
+
+    q = q->next;
+
+    while (q != NULL)
+    {
+        q = q->next;
+        p = p->next;
+    }
+
+    return p->next;
+}
+
+int main()
+{
+    node *head = NULL;
+
+    insertAtTail(head, 6);
+    insertAtTail(head, 3);
+    insertAtTail(head, 0);
+    insertAtTail(head, 5);
+    insertAtTail(head, 11);
+    insertAtTail(head, 4);
+    insertAtTail(head, 0);
+    insertAtTail(head, 6);
+    insertAtTail(head, 8);
+    insertAtTail(head, 7);
+    insertAtTail(head, 10);
+    insertAtTail(head, 9);
+    insertAtTail(head, 8);
+    display(head);
+
+    int k;
+    cin >> k;
+
+    cout << findkthNodefromLast(head, k)->data << endl;
+}
