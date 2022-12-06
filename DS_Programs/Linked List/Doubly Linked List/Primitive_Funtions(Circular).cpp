f6e11cc04c6638550f02f71cb205a025d37a3c53
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-06 08:22:31 +0530

    Create Primitive_Funtions(Circular).cpp

---
+++ DS_Programs/Linked List/Doubly Linked List/Primitive_Funtions(Circular).cpp
@@ -0,0 +1,238 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *prev;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, node *&prev, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        n->next = n;
+        n->prev = n;
+        head = n;
+        return;
+    }
+
+    node *temp = head->prev;
+
+    n->next = head;
+    head->prev = n;
+
+    temp->next = n;
+    n->prev = temp;
+    head = n;
+}
+
+void insertAtTail(node *&head, node *&prev, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        insertAtHead(head, prev, val);
+        return;
+    }
+
+    node *temp = head->prev;
+
+    n->next = head;
+    head->prev = n;
+
+    temp->next = n;
+    n->prev = temp;
+}
+
+void insertAfter(node *&head, node *&prev, node *p, int val)
+{
+    if (p == NULL)
+    {
+        insertAtHead(head, prev, val);
+        return;
+    }
+
+    node *n = new node(val);
+
+    node *q = p->next;
+    p->next = n;
+    n->prev = p;
+    n->next = q;
+    q->prev = n;
+}
+
+void insertBefore(node *&head, node *&prev, node *p, int val)
+{
+    if (p == NULL || p == head)
+    {
+        insertAtHead(head, prev, val);
+        return;
+    }
+
+    node *n = new node(val);
+
+    node *q = p->prev;
+    q->next = n;
+    n->prev = q;
+    n->next = p;
+    p->prev = n;
+}
+
+void deleteAtHead(node *&head, node *&prev)
+{
+    if (head == NULL)
+    {
+        cout << "Void Deletion" << endl;
+        return;
+    }
+
+    if (head->next == head)
+    {
+        node *todelete = head;
+        delete todelete;
+
+        head = NULL;
+
+        return;
+    }
+
+    node *todelete = head;
+
+    head->prev->next = head->next;
+    head->next->prev = head->prev;
+    head = head->prev->next;
+
+    delete todelete;
+}
+
+void deleteAtTail(node *&head, node *&prev)
+{
+    if (head == NULL)
+    {
+        cout << "Void Deletion" << endl;
+        return;
+    }
+
+    if (head->next == head)
+    {
+        node *todelete = head;
+        delete todelete;
+
+        head = NULL;
+
+        return;
+    }
+
+    node *todelete = head->prev;
+
+    head->prev->prev->next = head;
+    head->prev = head->prev->prev;
+
+    delete todelete;
+}
+
+void deleteBefore(node *&head, node *&prev, node *p)
+{
+    if (p == NULL || p->prev == NULL)
+    {
+        cout << "Void Deletion" << endl;
+        return;
+    }
+
+    if (p->prev->prev == NULL)
+    {
+        node *todelete = head;
+
+        p->prev = NULL;
+        head = head->next;
+
+        delete todelete;
+
+        return;
+    }
+
+    node *todelete = p->prev;
+    node *q = p->prev->prev;
+
+    p->prev = q;
+    q->next = p;
+
+    delete todelete;
+}
+
+void deleteAfter(node *&head, node *&prev, node *p)
+{
+    if (p == NULL)
+    {
+        cout << "Void Deletion" << endl;
+        return;
+    }
+
+    if (p->next == p && p->prev == p)
+    {
+        node *todelete = p;
+        delete todelete;
+
+        head = NULL;
+
+        return;
+    }
+
+    node *todelete = p->next;
+    node *q = p->next->next;
+
+    p->next = q;
+    q->prev = p;
+
+    delete todelete;
+}
+
+void display(node *head)
+{
+    if (head == NULL)
+    {
+        cout << "Linked List Is Empty" << endl;
+        return;
+    }
+
+    cout << "Linked List: ";
+    node *temp = head;
+
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+
+    } while (temp != head);
+
+    cout << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    node *prev = NULL;
+
+    insertAfter(head, prev, NULL, 11);
+    insertAfter(head, prev, head, 12);
+    insertAfter(head, prev, head->next, 15);
+    display(head);
+
+    deleteAfter(head, prev, head->prev);
+    deleteAfter(head, prev, head);
+    deleteAfter(head, prev, head);
+    display(head);
+}
