8b026001a5cc62a2dcc6f98f1431c4af45cb5487
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-10 08:04:07 +0530

    Create PostOrder_Traversal.cpp

---
+++ DS_Programs/Binary Tree/Traversal/PostOrder_Traversal.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void PostOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    PostOrder(root->left);
+    cout << root->data << " ";
+    PostOrder(root->right);
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    PostOrder(root);
+}
