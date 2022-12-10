7528921071ad9ee2d491ee2e78d6729e25e09f4d
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-10 10:36:03 +0530

    Create Sum_of_All_Nodes.cpp

---
+++ DS_Programs/Binary Tree/Sum_of_All_Nodes.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int SumOfAllNodes(node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }       
+
+    return SumOfAllNodes(root->left) + SumOfAllNodes(root->right) + root->data;
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    cout << SumOfAllNodes(root) << endl;
+}
