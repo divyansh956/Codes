c6382eff5f44e79f2d9c17cf36c04088b230dc7d
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-10 08:11:21 +0530

    Create Reverse_Traverse.cpp

---
+++ DS_Programs/Linked List/Reverse_Traverse.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    n->next = head;
+    head = n;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void reverseDisplay(node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    reverseDisplay(head->next);
+    cout << head->data << " ";
+}
+
+int main()
+{
+    node *head = NULL;
+
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+
+    reverseDisplay(head);
+}
