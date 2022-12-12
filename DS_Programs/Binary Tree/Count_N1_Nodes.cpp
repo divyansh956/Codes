a454e9149eb5b81e34b62ac9d08c8712ca7b16b4
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-12 10:08:07 +0530

    Create Count_N1_Nodes.cpp

---
+++ DS_Programs/Binary Tree/Count_N1_Nodes.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int count_N2_Nodes(node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 0;
+    }
+
+    if (root->left != NULL && root->right != NULL)
+    {
+        return count_N2_Nodes(root->left) + count_N2_Nodes(root->right);
+    }
+
+    return count_N2_Nodes(root->left) + count_N2_Nodes(root->right) + 1;
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    cout << count_N2_Nodes(root) << endl;
+}
