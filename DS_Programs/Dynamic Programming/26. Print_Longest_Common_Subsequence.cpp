c9f17a28cfb655765cc68e2a7d0f7f1b4dcb4b31
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-12 07:37:25 +0530

    Update 26. Print_Longest_Common_Subsequence.cpp

--- DS_Programs/Dynamic Programming/26. Print_Longest_Common_Subsequence.cpp
+++ DS_Programs/Dynamic Programming/26. Print_Longest_Common_Subsequence.cpp
@@ -6,6 +6,78 @@ void lcs(string s1, string s2)
     int n1 = s1.length();
     int n2 = s2.length();
 
+    vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+    for (int ind1 = 0; ind1 <= n1; ind1++)
+    {
+        dp[ind1][0] = 0;
+    }
+    for (int ind2 = 0; ind2 <= n2; ind2++)
+    {
+        dp[0][ind2] = 0;
+    }
+
+    for (int ind1 = 1; ind1 <= n1; ind1++)
+    {
+        for (int ind2 = 1; ind2 <= n2; ind2++)
+        {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+            {
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+            }
+            else
+            {
+                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+    }
+
+    string ans = "";
+
+    int ind1 = n1, ind2 = n2;
+    while (ind1 > 0 && ind2 > 0)
+    {
+        if (s1[ind1 - 1] == s2[ind2 - 1])
+        {
+            ans = s1[ind1 - 1] + ans;
+            ind1--;
+            ind2--;
+        }
+        else if (dp[ind1 - 1][ind2] > dp[ind1][ind2 - 1])
+        {
+            ind1--;
+        }
+        else
+        {
+            ind2--;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        lcs(s1, s2);
+    }
+}
+
+/****************************************************************************************************/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void lcs(string s1, string s2)
+{
+    int n1 = s1.length();
+    int n2 = s2.length();
+
     vector<int> prev(n2 + 1, 0);
     vector<int> curr(n2 + 1, 0);
 
