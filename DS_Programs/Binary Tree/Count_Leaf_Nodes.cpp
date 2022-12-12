b4b2fd499d93c6fbf9fd07067bf36fa503e51c98
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-12 09:36:23 +0530

    Create Count_Leaf_Nodes.cpp

---
+++ DS_Programs/Binary Tree/Count_Leaf_Nodes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int countLeafNodes(node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    cout << countLeafNodes(root) << endl;
+}
