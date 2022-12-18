008c81bdff60a497229688a62696ef6c92c3199d
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-18 17:21:23 +0530

    Create Top_View_Traversal.cpp

---
+++ DS_Programs/Binary Tree/Traversal/Top_View_Traversal.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void solve(node *root, int hd, map<int, vector<int>> &mp)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    mp[hd].push_back(root->data);
+
+    solve(root->left, hd - 1, mp);
+    solve(root->right, hd + 1, mp);
+}
+
+void topView(node *root)
+{
+    map<int, vector<int>> mp;
+    int hd = 0;
+
+    solve(root, hd, mp);
+
+    for (auto x : mp)
+    {
+        cout << x.second[0] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    topView(root);
+}
