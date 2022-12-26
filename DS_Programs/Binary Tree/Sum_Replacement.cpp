975da45ec6b3b5a8d3145fd115abfe323efad72d
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-26 22:11:58 +0530

    Create Sum_Replacement.cpp

---
+++ DS_Programs/Binary Tree/Sum_Replacement.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void sumReplace(node *&root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    sumReplace(root->left);
+    sumReplace(root->right);
+
+    if (root->left != NULL)
+    {
+        root->data += root->left->data;
+    }
+
+    if (root->right != NULL)
+    {
+        root->data += root->right->data;
+    }
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    preOrder(root);
+    cout << endl;
+
+    sumReplace(root);
+    preOrder(root);
+}
