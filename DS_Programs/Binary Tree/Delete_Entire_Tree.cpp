fa9c24a811676554195287801c89293ba7515b7e
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-26 12:35:01 +0530

    Create Delete_Entire_Tree.cpp

---
+++ DS_Programs/Binary Tree/Delete_Entire_Tree.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void PostOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    PostOrder(root->left);
+    PostOrder(root->right);
+    cout << root->data << " ";
+}
+
+void PreOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+
+void InOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    InOrder(root->left);
+    cout << root->data << " ";
+    InOrder(root->right);
+}
+
+node *deleteTree(node *&root)
+{
+    if (root == NULL)
+    {
+        delete root;
+        return NULL;
+    }
+
+    root->left = deleteTree(root->left);
+    root->right = deleteTree(root->right);
+    root = NULL;
+    delete root;
+
+    return root;
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    InOrder(root);
+    cout << endl;
+
+    node *t = deleteTree(root);
+    InOrder(t);
+    cout << endl;
+}
