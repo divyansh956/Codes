1405a2ee89094bf134e487131d8f4203e292aeb6
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-26 12:09:08 +0530

    Update Mirror_Image_Tree.cpp

--- DS_Programs/Binary Tree/Binary Search Tree/Mirror_Image_Tree.cpp
+++ DS_Programs/Binary Tree/Binary Search Tree/Mirror_Image_Tree.cpp
@@ -16,67 +16,28 @@ public:
     }
 };
 
-int search(string inOrder, int start, int end, int curr)
-{
-    for (int i = start; i <= end; i++)
-    {
-        if (curr == (inOrder[i] - '0'))
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-node*p=NULL;
-node *buildTree(string postOrder, string inOrder, int start, int end)
-{
-    static int idx = end;
-
-    if (start > end)
-    {
-        return NULL;
-    }
-
-    int curr = postOrder[idx] - '0';
-    idx--;
-
-    p = new node(curr);
-
-    if (start == end)
-    {
-        return p;
-    }
-
-    int pos = search(inOrder, start, end, curr);
-
-    p->right = buildTree(postOrder, inOrder, pos + 1, end);
-    p->left = buildTree(postOrder, inOrder, start, pos - 1);
-
-    return p;
-}
-
-void In(node *root, string &in)
+void PostOrder(node *root)
 {
     if (root == NULL)
     {
         return;
     }
 
-    In(root->left, in);
-    in += to_string(root->data);
-    In(root->right, in);
+    PostOrder(root->left);
+    PostOrder(root->right);
+    cout << root->data << " ";
 }
 
-void Pre(node *root, string &pre)
+void preOrder(node *root)
 {
     if (root == NULL)
     {
         return;
     }
 
-    pre += to_string(root->data);
-    Pre(root->left, pre);
-    Pre(root->right, pre);
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
 }
 
 void InOrder(node *root)
@@ -91,16 +52,78 @@ void InOrder(node *root)
     InOrder(root->right);
 }
 
-struct node *MirrorImage(node *root)
+void In(node *root, string &in)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    In(root->left, in);
+    in = to_string(root->data) + in;
+    In(root->right, in);
+}
+
+void Pre(node *root, string &post)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    post = to_string(root->data) + post;
+    Pre(root->left, post);
+    Pre(root->right, post);
+}
+
+int search(string inOrder, int start, int end, int curr)
+{
+    for (int i = start; i <= end; i++)
+    {
+        if (curr == (inOrder[i] - '0'))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+node *buildTree(string postOrder, string inOrder, int start, int end)
+{
+    static int idx = end;
+
+    if (start > end)
+    {
+        return NULL;
+    }
+
+    int curr = postOrder[idx] - '0';
+    idx--;
+
+    node *root = new node(curr);
+
+    if (start == end)
+    {
+        return root;
+    }
+
+    int pos = search(inOrder, start, end, curr);
+
+    root->right = buildTree(postOrder, inOrder, pos + 1, end);
+    root->left = buildTree(postOrder, inOrder, start, pos - 1);
+
+    return root;
+}
+
+node *MirrorImage(node *root)
 {
     string inOrder = "", postOrder = "";
 
     In(root, inOrder);
     Pre(root, postOrder);
-    reverse(postOrder.begin(), postOrder.end());
 
-    node *st = buildTree(postOrder, inOrder, 0, inOrder.length() - 1);
-    return st;
+    node *t = buildTree(postOrder, inOrder, 0, inOrder.length() - 1);
+    return t;
 }
 
 int main()
@@ -113,7 +136,8 @@ int main()
     root->right->left = new node(6);
     root->right->right = new node(7);
     InOrder(root);
+    cout << endl;
 
-    node *stt = MirrorImage(root);
-    InOrder(stt);
+    node *t = MirrorImage(root);
+    InOrder(t);
 }
