d5c402b947baa8b63b67dc0fd87694e7d1a8dc6b
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-26 10:32:02 +0530

    Create Copy_Of_Binary_Tree.cpp

---
+++ DS_Programs/Binary Tree/Copy_Of_Binary_Tree.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void PostOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    PostOrder(root->left);
+    PostOrder(root->right);
+    cout << root->data << " ";
+}
+
+void PreOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+
+void InOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    InOrder(root->left);
+    cout << root->data << " ";
+    InOrder(root->right);
+}
+
+node *copytree(node *t, node *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    t = root;
+    t->left = copytree(t, root->left);
+    t->right = copytree(t, root->right);
+
+    return t;
+}
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    InOrder(root);
+    cout << endl;
+
+    node *t = copytree(NULL, root);
+    InOrder(t);
+}
