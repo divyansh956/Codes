15912072538980e71608119a8b427165379a1352
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2022-12-26 20:05:29 +0530

    Update Build_Expression_Tree.cpp

--- DS_Programs/Binary Tree/Build_Expression_Tree.cpp
+++ DS_Programs/Binary Tree/Build_Expression_Tree.cpp
@@ -150,3 +150,169 @@ int main()
     PostOrder(root);
     cout << endl;
 }
+
+
+/**********************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    char data;
+    struct node *left;
+    struct node *right;
+    struct node *next;
+};
+
+struct node *MakeNode(char x)
+{
+    struct node *p;
+    p = (struct node *)malloc(sizeof(struct node));
+
+    p->data = x;
+    p->left = NULL;
+    p->right = NULL;
+    p->next = NULL;
+    
+    return p;
+}
+
+int prcd(char a, char b)
+{
+    if (a == '^' || a == '*' || a == '/' || a == '%')
+    {
+        if (b == '^')
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        if (a == '+' || a == '-')
+        {
+            if (b == '+' || b == '-')
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
+struct node *BuildExpTree(char *Exp)
+{
+    int i = 0;
+    char symb, op;
+
+    struct node *x, *y;
+    struct node *START = NULL, *p, *q;
+
+    char opStk[20];
+    int top = -1;
+
+    while (Exp[i] != '\0')
+    {
+        symb = Exp[i];
+        i++;
+
+        if (symb >= 'a' && symb <= 'z' || symb >= 'A' && symb <= 'Z' || symb >= '0' && symb <= '9')
+        {
+            x = MakeNode(symb);
+            x->next = START;
+            START = x;
+        }
+        else
+        {
+            while (top != -1 && prcd(opStk[top], symb))
+            {
+                op = opStk[top--];
+                y = MakeNode(op);
+
+                p = START;
+                START = START->next;
+
+                q = START;
+                START = START->next;
+
+                y->right = p;
+                y->left = q;
+
+                y->next = START;
+                START = y;
+            }
+            opStk[++top] = symb;
+        }
+    }
+
+    while (top != -1)
+    {
+        op = opStk[top--];
+        y = MakeNode(op);
+
+        p = START;
+        START = START->next;
+
+        q = START;
+        START = START->next;
+
+        y->right = p;
+        y->left = q;
+
+        y->next = START;
+        START = y;
+    }
+
+    return START;
+}
+
+void PreOrderTraversal(struct node *T)
+{
+    if (T != NULL)
+    {
+        printf("%c", T->data);
+        PreOrderTraversal(T->left);
+        PreOrderTraversal(T->right);
+    }
+}
+
+void InOrderTraversal(struct node *T)
+{
+    if (T != NULL)
+    {
+        InOrderTraversal(T->left);
+        printf("%c", T->data);
+        InOrderTraversal(T->right);
+    }
+}
+
+void PostOrderTraversal(struct node *T)
+{
+    if (T != NULL)
+    {
+        PostOrderTraversal(T->left);
+        PostOrderTraversal(T->right);
+        printf("%c", T->data);
+    }
+}
+
+int main()
+{
+    char Expression[20];
+    scanf("%s", Expression);
+
+    struct node *T = BuildExpTree(Expression);
+
+    PreOrderTraversal(T);
+    printf("\n\n");
+    InOrderTraversal(T);
+    printf("\n\n");
+    PostOrderTraversal(T);
+}
