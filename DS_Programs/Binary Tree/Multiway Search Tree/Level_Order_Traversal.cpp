fef0ec3c8eb39eb85e79f741719a46ca2daefd01
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2023-01-03 17:54:15 +0530

    Create Level_Order_Traversal.cpp

---
+++ DS_Programs/Binary Tree/Multiway Search Tree/Level_Order_Traversal.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    char data;
+    node *child;
+    node *sibling;
+
+    node(char val)
+    {
+        data = val;
+        child = NULL;
+        sibling = NULL;
+    }
+};
+
+void levelOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *p = q.front();
+        q.pop();
+
+        if (p != NULL)
+        {
+            cout << p->data << " ";
+
+            if (p->child != NULL)
+            {
+                q.push(p->child);
+            }
+
+            node *x = p->child;
+
+            while (x != NULL && x->sibling != NULL)
+            {
+                q.push(x->sibling);
+                x = x->sibling;
+            }
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+        }
+    }
+}
+
+void createTree(node *&root)
+{
+    int choice;
+
+    cout << "Whether The Child of " << root->data << " Exist(1/0): ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Enter Data For Child of " << root->data << ": ";
+        char x;
+        cin >> x;
+
+        node *n = new node(x);
+        root->child = n;
+
+        createTree(n);
+    }
+
+    cout << "Whether The Sibling of " << root->data << " Exist(1/0): ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Enter Data For Sibling of " << root->data << ": ";
+        char x;
+        cin >> x;
+
+        node *n = new node(x);
+        root->sibling = n;
+
+        createTree(n);
+    }
+}
+
+int main()
+{
+    node *root = NULL;
+
+    cout << "Enter Data For Root Node: ";
+    char x;
+    cin >> x;
+
+    root = new node(x);
+
+    createTree(root);
+    levelOrder(root);
+}
