d21e6489d4c64ab935a42a35d4cee364b33aa3be
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2023-01-03 17:43:11 +0530

    Create Convert_To_Binary_Tree.cpp

---
+++ DS_Programs/Binary Tree/Multiway Search Tree/Convert_To_Binary_Tree.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    char data;
+    node *child;
+    node *sibling;
+    node *left;
+    node *right;
+
+    node(char val)
+    {
+        data = val;
+        child = NULL;
+        sibling = NULL;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrder(root->child);
+    preOrder(root->sibling);
+}
+
+void BinaryTreelevelOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *p = q.front();
+        q.pop();
+
+        if (p != NULL)
+        {
+            cout << p->data << " ";
+
+            if (p->left != NULL)
+            {
+                q.push(p->left);
+            }
+
+            if (p->right != NULL)
+            {
+                q.push(p->right);
+            }
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+        }
+    }
+}
+
+void createTree(node *&root)
+{
+    int choice;
+
+    cout << "Whether The Child of " << root->data << " Exist(1/0): ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Enter Data For Child of " << root->data << ": ";
+        char x;
+        cin >> x;
+
+        node *n = new node(x);
+        root->child = n;
+
+        createTree(n);
+    }
+
+    cout << "Whether The Sibling of " << root->data << " Exist(1/0): ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Enter Data For Sibling of " << root->data << ": ";
+        char x;
+        cin >> x;
+
+        node *n = new node(x);
+        root->sibling = n;
+
+        createTree(n);
+    }
+}
+
+void convertBinaryTree(node *&root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (root->child != NULL)
+    {
+        root->left = root->child;
+        convertBinaryTree(root->child);
+    }
+
+    if (root->sibling != NULL)
+    {
+        root->right = root->sibling;
+        convertBinaryTree(root->sibling);
+    }
+}
+
+int main()
+{
+    node *root = NULL;
+
+    cout << "Enter Data For Root Node: ";
+    char x;
+    cin >> x;
+
+    root = new node(x);
+
+    createTree(root);
+    convertBinaryTree(root);
+    BinaryTreelevelOrder(root);
+}
