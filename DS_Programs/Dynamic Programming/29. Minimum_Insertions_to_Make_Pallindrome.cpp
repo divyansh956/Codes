2d714a1af37ca6b265331d33a31e18cc66bac0b4
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2023-01-26 12:08:59 +0530

    Create 29. Minimum_Insertions_to_Make_Pallindrome.cpp

---
+++ DS_Programs/Dynamic Programming/29. Minimum_Insertions_to_Make_Pallindrome.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int ind1, int ind2, string &s1, string &s2)
+{
+    if (ind1 < 0 || ind2 < 0)
+    {
+        return 0;
+    }
+
+    if (s1[ind1] == s2[ind2])
+    {
+        return 1 + solve(ind1 - 1, ind2 - 1, s1, s2);
+    }
+
+    return max(solve(ind1 - 1, ind2, s1, s2), solve(ind1, ind2 - 1, s1, s2));
+}
+
+int lcs(string s, string t)
+{
+    int n1 = s.length();
+    int n2 = t.length();
+
+    return solve(n1 - 1, n2 - 1, s, t);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        cin >> s1;
+
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());
+
+        cout << s1.length() - lcs(s1, s2) << endl;
+    }
+}
+
+/*********************************************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int ind1, int ind2, string &s1, string &s2, vector<vector<int>> &dp)
+{
+    if (ind1 == 0 || ind2 == 0)
+    {
+        return 0;
+    }
+
+    if (dp[ind1][ind2] != -1)
+    {
+        return dp[ind1][ind2];
+    }
+
+    if (s1[ind1 - 1] == s2[ind2 - 1])
+    {
+        return dp[ind1][ind2] = 1 + solve(ind1 - 1, ind2 - 1, s1, s2, dp);
+    }
+
+    return dp[ind1][ind2] = max(solve(ind1 - 1, ind2, s1, s2, dp), solve(ind1, ind2 - 1, s1, s2, dp));
+}
+
+int lcs(string s, string t)
+{
+    int n1 = s.length();
+    int n2 = t.length();
+
+    vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+
+    return solve(n1, n2, s, t, dp);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        cin >> s1;
+
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());
+
+        cout << s1.length() - lcs(s1, s2) << endl;
+    }
+}
+
+/*********************************************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs(string s1, string s2)
+{
+    int n1 = s1.length();
+    int n2 = s2.length();
+
+    vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+    for (int ind1 = 0; ind1 <= n1; ind1++)
+    {
+        dp[ind1][0] = 0;
+    }
+    for (int ind2 = 0; ind2 <= n2; ind2++)
+    {
+        dp[0][ind2] = 0;
+    }
+
+    for (int ind1 = 1; ind1 <= n1; ind1++)
+    {
+        for (int ind2 = 1; ind2 <= n2; ind2++)
+        {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+            {
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+            }
+            else
+            {
+                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+    }
+
+    return dp[n1][n2];
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        cin >> s1;
+
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());
+
+        cout << s1.length() - lcs(s1, s2) << endl;
+    }
+}
+
+/*********************************************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs(string s1, string s2)
+{
+    int n1 = s1.length();
+    int n2 = s2.length();
+
+    vector<int> prev(n2 + 1, 0);
+    for (int ind2 = 0; ind2 <= n2; ind2++)
+    {
+        prev[ind2] = 0;
+    }
+
+    for (int ind1 = 1; ind1 <= n1; ind1++)
+    {
+        vector<int> curr(n2 + 1, 0);
+
+        for (int ind2 = 1; ind2 <= n2; ind2++)
+        {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+            {
+                curr[ind2] = 1 + prev[ind2 - 1];
+            }
+            else
+            {
+                curr[ind2] = max(prev[ind2], curr[ind2 - 1]);
+            }
+        }
+        prev = curr;
+    }
+
+    return prev[n2];
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        cin >> s1;
+
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());
+
+        cout << s1.length() - lcs(s1, s2) << endl;
+    }
+}
